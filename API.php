3979f384703b7e2feea87b031d933f641e1bd1f7
globbook <team@globbook.com>
2025-02-14 00:13:08 +0530

    Create API.php

---
+++ API.php
@@ -0,0 +1,95 @@
+<?php
+class GlobbookAuthAPI {
+    private $app_id;
+    private $app_secret;
+    private $access_token;
+    private $base_url = "https://globbook.com/api";
+    public string $version = "1.0";
+
+    public function __construct($app_id = null, $app_secret = null) {
+        $this->app_id = $app_id ?: getenv('GB_APP_ID');
+        $this->app_secret = $app_secret ?: getenv('GB_APP_SECRET');
+    }
+
+    private function request($endpoint, $params = [], $method = 'GET') {
+        $url = $this->base_url . $endpoint;
+        $options = [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 30,
+        ];
+
+        if ($method == 'POST') {
+            $options[CURLOPT_POST] = true;
+            $options[CURLOPT_POSTFIELDS] = http_build_query($params);
+        } else {
+            $url .= '?' . http_build_query($params);
+        }
+
+        $options[CURLOPT_URL] = $url;
+        $ch = curl_init();
+        curl_setopt_array($ch, $options);
+
+        $response = curl_exec($ch);
+        if ($response === false) {
+            throw new Exception('cURL error: ' . curl_error($ch));
+        }
+        curl_close($ch);
+
+        $data = json_decode($response, true);
+        if (isset($data['error'])) {
+            throw new Exception('API error: ' . $data['error']);
+        }
+
+        return $data;
+    }
+
+    public function authenticate($auth_key) {
+        try {
+            $response = $this->request("/authorize", [
+                "app_id" => $this->app_id,
+                "app_secret" => $this->app_secret,
+                "auth_key" => $auth_key
+            ], 'POST');
+            
+            if (!empty($response['access_token'])) {
+                $this->access_token = $response['access_token'];
+                return $this->access_token;
+            }
+            
+            return null;
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+
+    public function getUserInfo() {
+        if (!$this->access_token) {
+            throw new Exception("Access token is not set. Authenticate first.");
+        }
+
+        try {
+            $userInfo = $this->request("/get_user_info", ["access_token" => $this->access_token]);
+            
+            if (!empty($userInfo['user_info'])) {
+                return (object) [
+                    'username' => $userInfo['user_info']['user_name'] ?? '',
+                    'email' => $userInfo['user_info']['user_email'] ?? '',
+                    'firstname' => $userInfo['user_info']['user_firstname'] ?? '',
+                    'lastname' => $userInfo['user_info']['user_lastname'] ?? '',
+                    'gender' => $userInfo['user_info']['user_gender'] ?? '',
+                    'birthdate' => $userInfo['user_info']['user_birthdate'] ?? '',
+                    'picture' => $userInfo['user_info']['user_picture'] ?? '',
+                    'cover' => $userInfo['user_info']['user_cover'] ?? '',
+                    'verified' => $userInfo['user_info']['user_verified'] ?? '',
+                    'relationship' => $userInfo['user_info']['user_relationship'] ?? '',
+                    'biography' => $userInfo['user_info']['user_biography'] ?? '',
+                    'website' => $userInfo['user_info']['user_website'] ?? '',
+                ];
+            }
+            
+            return null;
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+}
